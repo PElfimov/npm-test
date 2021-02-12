9f0a12d89b4b87199291042ac9a0add0c9469bfe
Pavel Elfimov <pelfimov@yandex.ru>
2021-02-12 17:14:16 +0700

    fix read line

--- readline.js
+++ readline.js
@@ -10,16 +10,25 @@ rl.prompt();
 
 rl.on(`line`, (line) => {
   line = line.trim();
+
+  switch (line) {
+    case `hello`:
+      console.log(`world`);
+      break;
+    case `bye`:
+      rl.close();
+      return;
+    default:
+      console.log(`Say what? I might have heard '${line}'`);
+      break;
+  }
 });
 
-switch (line) {
-  case `hello`:
-    console.log(`world`);
-    break;
-  case `bye`:
-    rl.close();
-    return;
-  default:
-    console.log(`Say what? I might have heard '${line}'`);
-    break;
-}
+rl.prompt();
+rl.on(`close`, () => {
+  console.log(`Have a great day!`);
+  process.exit();
+}).on(`error`, (err) => {
+  console.log(err);
+  process.exit(1);
+});
