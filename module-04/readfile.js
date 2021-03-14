291cf65042275cbfc0da762bd5f5638811d08acc
Pavel Elfimov <pelfimov@yandex.ru>
2021-03-14 12:09:55 +0700

    add new file fore module 4

---
+++ module-04/readfile.js
@@ -0,0 +1,30 @@
+const fs = require(`fs`);
+const { promisify } = require(`util`);
+
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+const unlink = promisify(fs.unlink);
+const mkdir = promisify(fs.mkdir);
+const rmdir = promisify(fs.rmdir);
+
+const readWrite = (did) => {
+  const path = `${dir}/newfile.txt`;
+
+  let exist = false;
+  mkdir(dir)
+    .catch((e) => {
+      if (e.code === `EEXIST`) {
+        exist = true;
+        return Promise.resolve();
+      }
+      return Promise.reject(e);
+    })
+    .then(() => writeFile(path, `Hello, world!`))
+    .then(() => readFile(path))
+    .then((data) => console.log(data.toString()))
+    .then(() => unlink(path))
+    .then(() => (exist ? Promise.resolve() : rmdir(dir)))
+    .catch((e) => console.error(e));
+};
+
+readWrite(`${__dirname}/test`);
