4b00e6aa6d9fe633a27761d44dd1c471f40e72d9
Pavel Elfimov <pelfimov@yandex.ru>
2021-03-27 18:28:41 +0700

    add first test

--- test/get-wizards.js
+++ test/get-wizards.js
@@ -5,13 +5,22 @@ const {app} = require(`../src/server`);
 describe(`GET /api/wizards`, function () {
   it(`respond with json`, () => {
     return request(app)
-      .get(`/api/wiards`)
+      .get(`/api/wizards`)
       .set(`Accept`, `application/json`)
       .expect(200)
-      .then((response) => {
-        const wizards = response.body;
-        assert.equal(wizards.length, 17);
-        assert.equal(Object.keys(wizards[0]).length, 5);
-      });
+      .expect(`Content-Type`, /json/)
+    .then((response) => {
+      const wizards = response.body;
+      assert.equal(wizards.length, 17);
+      assert.equal(Object.keys(wizards[0]).length, 5);
+    });
+  });
+
+  it(`unknown address should respond with 404`, ()=>{
+    return request(app)
+    .get(`/api/wizardsss`)
+    .set(`Accept`, `application/json`)
+    .expect(404)
+    .expect(`Content-Type`, /html/);
   });
 });
