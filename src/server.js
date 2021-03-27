0b51417c9e4537ee28aa783343e45f100da2d76c
Pavel Elfimov <pelfimov@yandex.ru>
2021-03-27 15:43:46 +0700

    add new route api\wizars

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require(`express`);
+const { generate: generateWizards } = require(`./generator/wizards-generator`);
 
 const app = express();
 app.use(express.static(`static`));
+
+const wizards = generateWizards();
+app.get(`/api/wizards`, (req, res) => {
+  res.send(wizards);
+});
 
 const HOSTNAME = `127.0.0.1`;
 const PORT = 3000;
