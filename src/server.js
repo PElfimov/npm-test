e7a4c39447b7067e5aeaa6b27fe6c2ed218e6374
Pavel Elfimov <pelfimov@yandex.ru>
2021-03-31 18:37:13 +0700

    fix test

--- src/server.js
+++ src/server.js
@@ -1,23 +1,32 @@
 const express = require(`express`);
 const {generate: generateWizards} = require(`./generator/wizards-generator`);
+const bodyParser = require(`body-parser`);
+const multer = require(`multer`);
 
 const app = express();
 app.use(express.static(`static`));
+app.use(bodyParser.json());
+
+const upload = multer({storage: multer.memoryStorage()});
 
 const wizards = generateWizards();
 app.get(`/api/wizards`, (req, res) => {
   res.send(wizards);
 });
 
-app.get(`/api/wizards/:name`, (req, res)=>{
+app.get(`/api/wizards/:name`, (req, res) => {
   const name = req.params[`name`].toLocaleLowerCase();
-  const wizard = wizards.find((it)=>it.name.toLocaleLowerCase() === name);
+  const wizard = wizards.find((it) => it.name.toLocaleLowerCase() === name);
 
   if (!wizard) {
     res.status(404).end();
   } else {
-    res.sendStatus(wizard);
+    res.send(wizard);
   }
+});
+
+app.post(`/api/wizards`, upload.none(), (req, res) => {
+  res.send(req.body);
 });
 
 const HOSTNAME = `127.0.0.1`;
--- test/post-wizard.js
+++ test/post-wizard.js
@@ -3,31 +3,35 @@ const assert = require(`assert`);
 const {app} = require(`../src/server`);
 
 describe(`POST /api/wizards`, function () {
-
-  it(`should consume JSON`, ()=>{
-    return request(app).post(`/api/wizards`).
-      send({
+  it(`should consume JSON`, () => {
+    return request(app)
+      .post(`/api/wizards`)
+      .send({
         name: `Гендальф Серый`,
         colorCoat: `rgb(56, 159, 117)`,
         colorEyes: `red`,
-        colorFireball: `#5ce6c0`
-      }).
-      expect(200).
-      then((response)=>{
-        const wizards = response.body;
-        assert.equal(wizards.length, 17);
-        assert.equal(Object.keys(wizards[0]).length, 5);
+        colorFireball: `#5ce6c0`,
+      })
+      .expect(200, {
+        name: `Гендальф Серый`,
+        colorCoat: `rgb(56, 159, 117)`,
+        colorEyes: `red`,
+        colorFireball: `#5ce6c0`,
       });
   });
 
-  it(`should consume form-data`, ()=>{
-    return request(app).post(`/api/wizards`).
-      field(`name`, `Гендальф Серый`).
-      expect(200).
-      then((response)=>{
-        const wizards = response.body;
-        assert.equal(wizards.length, 17);
-        assert.equal(Object.keys(wizards[0]).length, 5);
+  it(`should consume form-data`, () => {
+    return request(app)
+      .post(`/api/wizards`)
+      .field(`name`, `Гендальф Серый`)
+      .field(`colorCoat`, `rgb(56, 159, 117)`)
+      .field(`colorEyes`, `red`)
+      .field(`colorFireball`, `#5ce6c0`)
+      .expect(200, {
+        name: `Гендальф Серый`,
+        colorCoat: `rgb(56, 159, 117)`,
+        colorEyes: `red`,
+        colorFireball: `#5ce6c0`,
       });
   });
 });
