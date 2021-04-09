2b3bbcf134d67860a66a10c02b4d6a5606850363
Pavel Elfimov <pelfimov@yandex.ru>
2021-04-09 09:20:42 +0700

    add some code

--- module-06/connect.js
+++ module-06/connect.js
@@ -3,8 +3,8 @@ const {MongoClient} = require(`mongodb`);
 const url = `mongodb://localhost:27017`;
 
 const connectAndRead = async () => {
-  const client = await MongoClient.connect(url);
-  const db = client.db(`my-fancy-game`);
+  const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+  const db = client.db(`wizard-game`);
 
   const collection = db.collection(`wizard`);
   console.log(collection);
