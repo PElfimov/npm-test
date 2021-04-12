dc880b1fc8d36f6c1cf3562deed6b056887ac2c1
Pavel Elfimov <pelfimov@yandex.ru>
2021-04-12 19:32:55 +0700

    add skip

--- src/server/wizards/route.js
+++ src/server/wizards/route.js
@@ -24,84 +24,100 @@ const upload = multer({storage: multer.memoryStorage()});
 
 const toPage = async (cursor, skip = 0, limit = 20) => {
   return {
-    data: await (cursor.skip(skip).limit(limit).toArray()),
+    data: await cursor.skip(skip).limit(limit).toArray(),
     skip,
     limit,
-    total: await cursor.count()
+    total: await cursor.count(),
   };
 };
 
-wizardsRouter.get(``, async(async (req, res) => res.send(await toPage(await wizardsRouter.wizardsStore.getAllWizards()))));
+wizardsRouter.get(
+    ``,
+    async(async (req, res) =>
+      res.send(
+          await toPage(await wizardsRouter.wizardsStore.getAllWizards(), parseInt(req.query.skip, 10))
+      )
+    )
+);
 
-wizardsRouter.post(``, upload.single(`avatar`), async(async (req, res) => {
-  const data = req.body;
+wizardsRouter.post(
+    ``,
+    upload.single(`avatar`),
+    async(async (req, res) => {
+      const data = req.body;
 
-  const avatar = req.file;
-  if (avatar) {
-    data.avatar = avatar;
-  }
-  logger.info(`Received data from user: `, data);
-  const errors = validateSchema(data, codeAndMagicSchema);
+      const avatar = req.file;
+      if (avatar) {
+        data.avatar = avatar;
+      }
+      logger.info(`Received data from user: `, data);
+      const errors = validateSchema(data, codeAndMagicSchema);
 
-  if (errors.length > 0) {
-    throw new ValidationError(errors);
-  }
+      if (errors.length > 0) {
+        throw new ValidationError(errors);
+      }
 
-  if (avatar) {
-    const avatarInfo = {
-      path: `/api/wizards/${data.username}/avatar`,
-      mimetype: avatar.mimetype
-    };
-    await wizardsRouter.imageStore.save(avatarInfo.path, createStreamFromBuffer(avatar.buffer));
-    data.avatar = avatarInfo;
-  }
+      if (avatar) {
+        const avatarInfo = {
+          path: `/api/wizards/${data.username}/avatar`,
+          mimetype: avatar.mimetype,
+        };
+        await wizardsRouter.imageStore.save(avatarInfo.path, createStreamFromBuffer(avatar.buffer));
+        data.avatar = avatarInfo;
+      }
 
-  await wizardsRouter.wizardsStore.save(data);
-  dataRenderer.renderDataSuccess(req, res, data);
-}));
+      await wizardsRouter.wizardsStore.save(data);
+      dataRenderer.renderDataSuccess(req, res, data);
+    })
+);
 
-wizardsRouter.get(`/:name`, async(async (req, res) => {
-  const wizardName = req.params.name;
+wizardsRouter.get(
+    `/:name`,
+    async(async (req, res) => {
+      const wizardName = req.params.name;
 
-  const found = await wizardsRouter.wizardsStore.getWizard(wizardName);
-  if (!found) {
-    throw new NotFoundError(`Wizard with name "${wizardName}" not found`);
-  }
-  res.send(found);
-}));
+      const found = await wizardsRouter.wizardsStore.getWizard(wizardName);
+      if (!found) {
+        throw new NotFoundError(`Wizard with name "${wizardName}" not found`);
+      }
+      res.send(found);
+    })
+);
 
-wizardsRouter.get(`/:name/avatar`, async(async (req, res) => {
-  const wizardName = req.params.name;
+wizardsRouter.get(
+    `/:name/avatar`,
+    async(async (req, res) => {
+      const wizardName = req.params.name;
 
-  const wizard = await wizardsRouter.wizardsStore.getWizard(wizardName);
+      const wizard = await wizardsRouter.wizardsStore.getWizard(wizardName);
 
-  if (!wizard) {
-    throw new NotFoundError(`Wizard with name "${wizardName}" not found`);
-  }
+      if (!wizard) {
+        throw new NotFoundError(`Wizard with name "${wizardName}" not found`);
+      }
 
-  const avatar = wizard.avatar;
+      const avatar = wizard.avatar;
 
-  if (!avatar) {
-    throw new NotFoundError(`Wizard with name "${wizardName}" didn't upload avatar`);
-  }
+      if (!avatar) {
+        throw new NotFoundError(`Wizard with name "${wizardName}" didn't upload avatar`);
+      }
 
-  const {info, stream} = await wizardsRouter.imageStore.get(avatar.path);
+      const {info, stream} = await wizardsRouter.imageStore.get(avatar.path);
 
-  if (!info) {
-    throw new NotFoundError(`File was not found`);
-  }
+      if (!info) {
+        throw new NotFoundError(`File was not found`);
+      }
 
-  res.set(`content-type`, avatar.mimetype);
-  res.set(`content-length`, info.length);
-  res.status(200);
-  stream.pipe(res);
-}));
+      res.set(`content-type`, avatar.mimetype);
+      res.set(`content-length`, info.length);
+      res.status(200);
+      stream.pipe(res);
+    })
+);
 
 wizardsRouter.use((exception, req, res, next) => {
   dataRenderer.renderException(req, res, exception);
   next();
 });
-
 
 module.exports = (wizardStore, imageStore) => {
   wizardsRouter.wizardsStore = wizardStore;
