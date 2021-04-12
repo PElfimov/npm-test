ed0413fd12802ad774c24e8449376d2767e7d88e
Pavel Elfimov <pelfimov@yandex.ru>
2021-04-12 15:41:36 +0700

    replase server

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,23 @@
 const express = require(`express`);
-const wizardsRouter = require(`./wizards/route`);
+const wizardStore = require(`./wizards/store`);
+const imageStore = require(`./images/store`);
+const wizardsRouter = require(`./wizards/route`)(wizardStore, imageStore);
+const logger = require(`../logger`);
 
 const app = express();
 app.use(express.static(`static`));
+
 app.use(`/api/wizards`, wizardsRouter);
 
-const HOSTNAME = `127.0.0.1`;
-const PORT = 3000;
+const HOSTNAME = process.env.SERVER_HOST || `localhost`;
+const PORT = parseInt(process.env.SERVER_PORT, 10) || 3000;
 
 const serverAddress = `http://${HOSTNAME}:${PORT}`;
-
 module.exports = {
   run() {
     app.listen(PORT, HOSTNAME, () => {
-      console.log(`Server running at ${serverAddress}/`);
+      logger.info(`Server running at ${serverAddress}/`);
     });
   },
-  app,
+  app
 };
