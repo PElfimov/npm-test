a1af59acf9a0703004c0adb612ca5d98f69d4322
Pavel Elfimov <pelfimov@yandex.ru>
2021-04-12 15:45:34 +0700

    add database

---
+++ src/database/database.js
@@ -0,0 +1,9 @@
+const {MongoClient} = require(`mongodb`);
+const logger = require(`../logger`);
+
+const url = process.env.MONGO_URL || `mongodb://localhost:27017`;
+
+module.exports = MongoClient.connect(url).then((client) => client.db(`code-and-magick`)).catch((e) => {
+  logger.error(`Failed to connect to MongoDB`, e);
+  process.exit(1);
+});
